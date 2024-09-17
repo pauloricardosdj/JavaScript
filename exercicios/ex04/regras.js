2369f257d24c137d896f084f5e1e04ad02a54f77
Paulo Ricardo <158747347+pauloricardosdj@users.noreply.github.com>
2024-09-17 12:12:48 -0300

    regras

---
+++ exercicios/ex04/regras.js
@@ -0,0 +1,19 @@
+let nomeproduto = "boné";
+let precoproduto = 50;
+let quantidade = 3;
+
+let minhaprimeiramensagem = "Olá, Bem Vindo!"; // Camel case
+// const const = "Isso não é uma boa prática";
+
+let _email = "email@email.com.br";
+let funcionario_email = "email@outroemail.com.br";
+
+let mensagem = "Olá Teste";
+
+let segundamensagem = 'Olá teste novamente';
+
+let texto = "Eu quero dar ênfase na próxima palavra: 'Essa'";
+let texto2 = 'Eu quero dar ênfase na próxima palavra: "Essa"'; // Menos utilizada
+
+const PI = 3.14;
+const MAX_WIDTH = 769;
