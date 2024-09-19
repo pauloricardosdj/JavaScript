ccc5dc4aa86a87fa827b3972f782c22b457b6c0c
Paulo Ricardo <158747347+pauloricardosdj@users.noreply.github.com>
2024-09-19 09:09:50 -0300

    Create script.js

---
+++ exercicios/ex05/script.js
@@ -0,0 +1,28 @@
+// String
+
+// "Um texto" - string
+// 'Criar uma mensagem' - string
+// `Cria mais um texto` - string
+
+let mensagem = 'Olá, ';
+let nome = 'Paulo';
+let mensagem3 = 'Bem vindo ao clube!';
+let mensagem2 = `Olá, ${nome}. ${mensagem3}`;
+
+console.log(mensagem + nome + '.Bem vindo ao clube!');
+console.log(mensagem2);
+
+// indice = 0
+console.log((nome[3] = 'Q'));
+console.log(nome);
+
+// Number
+let numero = 30;
+let numero1 = -56;
+let decimal = 6.78;
+console.log(numero, numero1, decimal);
+// Infinity e NaN
+
+let string = '10';
+let dez = 10;
+console.log(`${string} + ${dez}`);
