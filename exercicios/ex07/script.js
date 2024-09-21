8f4ccf7db7c84d7d68591341a14ed4f40154ca72
Paulo Ricardo <158747347+pauloricardosdj@users.noreply.github.com>
2024-09-21 11:25:07 -0300

    undefined

---
+++ exercicios/ex07/script.js
@@ -0,0 +1,13 @@
+let mensagem = "Olá, Programador";
+let preco = 50.0;
+let logado = true;
+
+// Undefined
+let produto;
+let carrinho = undefined;
+console.log(typeof produto);
+console.log(typeof carrinho);
+// NUll
+let lista = null;
+console.log(lista);
+console.log(typeof lista);
