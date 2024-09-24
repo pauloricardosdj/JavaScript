aefcbea2b73c74ccf5f077a12f92fff12b511c01
Paulo Ricardo <158747347+pauloricardosdj@users.noreply.github.com>
2024-09-24 09:08:38 -0300

    valores aritimeticos

---
+++ exercicios/ex09/script.js
@@ -0,0 +1,25 @@
+// Adição
+let valor = 10;
+let segundovalor = 43;
+
+console.log(valor + segundovalor);
+
+// let mensagem = "20";
+// console.log(mensagem + valor); // concatenação
+
+// Subtração
+console.log( valor - segundovalor);
+console.log(segundovalor - valor);
+
+// Divisão
+console.log(valor / segundovalor);
+console.log(segundovalor / valor);
+
+// Módulo
+console.log(valor % segundovalor);
+console.log(segundovalor % valor);
+
+let modulo = 9 % 2;
+let divisao = 10 / 2;
+console.log(modulo);
+console.log(divisao);
---
+++ exercicios/ex10/script.js
@@ -0,0 +1,15 @@
+// Adição + Subtração - Multiplicação * Divisão / Módulo % Inversão de sinal/Negativo -
+
+// Operadores Aritméticos avançados
+//exponenciação **
+let base = 2;
+let expoente = 3;
+let resultado = base ** expoente; // 2 seja elevado a 3
+console.log(resultado);
+
+// Incremento ++ (adicionar o valor 1) Decremento -- (diminuir o valor 1)
+let controle = 0;
+console.log(controle);
+console.log(--controle);
+console.log(--controle);
+console.log(--controle);
