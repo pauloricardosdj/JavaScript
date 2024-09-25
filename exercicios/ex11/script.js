1528e64dc131ea33a5cf8dda6483a53e62b18502
Paulo Ricardo <158747347+pauloricardosdj@users.noreply.github.com>
2024-09-25 10:23:04 -0300

    .

---
+++ exercicios/ex11/script.js
@@ -0,0 +1,14 @@
+//Igualdae e Estrita Igualdade
+console.log(5 == "5");
+console.log(5 === "5"); // boa prática
+
+console.log(5 != "5");
+console.log(5 !== "5"); // boa prática
+
+//Maior e Menor
+console.log(10 <= 10); // False
+
+let senha = 12345;
+let senhadigitada = 12345;
+let comparaçao = senha === senhadigitada;
+console.log("Aqui é minha senha " + comparaçao)
---
+++ exercicios/ex12/script.js
@@ -0,0 +1,8 @@
+//Coerção Implícita
+console.log(5 == "5"); // Igualdade - coerção implicita
+console.log(5 === "5");
+console.log(5 + "5"); // Número é convertido para string
+console.log("10" - 5); // String convertida para number
+console.log("3" * "2"); // Strings são convertidas para number
+
+console.log(null = undefined);
