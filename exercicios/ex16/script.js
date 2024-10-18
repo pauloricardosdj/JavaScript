469f06a773538fb10cef32df8094cf0361f48256
Paulo Ricardo <158747347+pauloricardosdj@users.noreply.github.com>
2024-10-17 21:51:48 -0300

    Create script.js

---
+++ exercicios/ex16/script.js
@@ -0,0 +1,14 @@
+// Aray
+let lista = ["Banana", 23, true, "Maçã"]; // indice 0
+
+lista = ["banana", "maça" , "pera"];
+
+console.log(lista);
+lista[0] = "laranja";
+console.log(lista[0]);
+console.log(lista);
+
+let nome = "João";
+nome[0] = "M"
+
+console.log(nome);
