db134f054b6dac8e601d8d7db68855a20db03ee8
Paulo Ricardo <158747347+pauloricardosdj@users.noreply.github.com>
2024-11-02 19:05:48 -0300

    Update script.js

--- exercicios/praticas/ex02/script.js
+++ exercicios/praticas/ex02/script.js
@@ -22,3 +22,22 @@ function calcmediafinal (n1, n2, n3, notapf) {
 }
 
 calcmediafinal(9, 7, 3, 9);
+
+//  3 - Escreva uma função que recebe do usuário um valor de temperatura em graus Celsius e escreve na tela o texto: a temperatura correspondente em Farenheit é de xºF (sendo x o resultado da conversão.)
+//  Obs: temperatura em Farenheit = (9/5)*( temperatura em Celsius ) + 32
+
+function temperatura (temp) {
+    const tempfht = (9 / 5) * temp + 32;
+    console.log(`A temperatura em Farenheit é de: ${tempfht}`)
+}
+
+temperatura(10);
+
+// 4 - Quando estava passeando em Nova York, Bruno se surpreendeu com uma realidade diferente da  que conhecia. Ao tentar pagar um produto, ele percebeu que o valor cobrado era sempre um pouco maior do que o que estava presente na etiqueta da prateleira onde o achou. Isso acontece porque a etiqueta não contém o valor do imposto pago sobre o produto, que é calculado no caixa. Esse imposto é de 8,875% em cima do valor do produto. Faça uma função que recebe o valor do produto presente na etiqueta e registre na tela a frase: O valor a ser pago é x (sendo x o resultado do acréscimo dos impostos sobre o valor inicial)
+
+function brunopagaimposto(valordaetiqueta) {
+    const valorcomimposto = 1.08875 * valordaetiqueta;
+    console.log(`O valor a ser pago é ${valorcomimposto}`)
+}
+
+brunopagaimposto(10);
