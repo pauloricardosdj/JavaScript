5ede431da7faa3cdf5ad2da905f8429be05510e9
Paulo Ricardo <158747347+pauloricardosdj@users.noreply.github.com>
2024-11-02 18:30:05 -0300

    Update script.js

--- exercicios/praticas/ex02/script.js
+++ exercicios/praticas/ex02/script.js
@@ -6,3 +6,19 @@ function calcmedia(n1, n2, n3) {
 }
 
 calcmedia(9, 7, 3);
+
+// 2 - Imagine agora que o aluno do item anterior não atingiu a média mínima pra passar, e por isso está de prova final. Faça uma nova função que calcula a média final desse aluno a partir de 4 notas, as das 3 primeiras provas + a nota da prova final. O cálculo deve ser o seguinte
+
+function calcmediaadp (n1, n2, n3) {
+    const medianota1 = (n1 + n2 + n3)/ 3;
+    return medianota1;
+}
+
+function calcmediafinal (n1, n2, n3, notapf) {
+    const mediasempf = calcmediaadp(n1, n2, n3);
+    const mediafinal = (mediasempf + notapf) / 2;
+    console.log(`A média final desse aluno é: ${mediafinal}`);
+
+}
+
+calcmediafinal(9, 7, 3, 9);
